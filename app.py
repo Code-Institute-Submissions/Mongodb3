924f456e793e92211c9be871672c9afbae3bff77
waltercarreno <walter_tavernes14@hotmail.com>
2021-12-10 22:21:53 +0000

    Changes make app.py. Users cannot longer edit or delete recipes if they are not login or not are the creators

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from flask_pymongo import PyMongo
 from flask_paginate import Pagination
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
-from forms import RegisterForm, LoginForm, RecipeForm, EditRecipeForm, DeleteForm
+from forms import RegisterForm, LoginForm, RecipeForm, EditRecipeForm,\
+ DeleteForm
 if os.path.exists("env.py"):
     import env
 
@@ -20,82 +21,107 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-def get_recipes(offset=0, per_page=5):
+def find_recipes(offset=0, per_page=5):
+    """
+    function for pagination
+    """
     return mongo.db.recipes.find()[offset: offset + per_page]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
+    """
+    function for home extent jinja base.html
+    """
     return render_template("home.html")
 
 
 @app.route('/allrecipes')
 def allrecipes():
+    """
+    function for allrecipes extent jinja base.html.
+    Users can find allrecipes upload by other users.
+    """
     page = int(request.args.get('page', 1))
     per_page = 2
     offset = (page - 1) * per_page
-    paginated_recipes = get_recipes(offset=offset, per_page=per_page)
-    total=paginated_recipes.count()
-    pagination = Pagination(page=page, per_page=per_page, total=total)
-    return render_template( "allrecipes.html", recipes=paginated_recipes,
-                            page=page,
-                            per_page=per_page,
-                            pagination=pagination)
+    paginated_recipes = find_recipes(offset=offset, per_page=per_page)
+    total = paginated_recipes.count()
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='materialize')
+    return render_template("allrecipes.html", recipes=paginated_recipes,
+                           page=page, per_page=per_page,
+                           pagination=pagination)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """
+    function for login extent jinja base.html.
+    Users can  access website.
+    """
     login_form = LoginForm()
 
     if login_form.validate_on_submit():
-        found_username = mongo.db.users.find_one({'username': request.form['username']})
+        found_username = mongo.db.users.find_one({'username':
+                                                  request.form['username']})
 
         if found_username:
-            existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username")})
-            if check_password_hash(
-                existing_user["password"], request.form.get("password")):
+            existing_user = mongo.db.users.find_one({"username":
+                                                     request.form.get(
+                                                      "username")})
+            if check_password_hash(existing_user["password"],
+                                   request.form.get("password")):
                 session["username"] = request.form.get("username")
                 session['logged-in'] = True
-                flash(f'Login successfully.', 'primary')
-                return redirect(url_for("profile", username=session["username"]))
+                flash('Login successfully.', 'primary')
+                return redirect(url_for("profile",
+                                        username=session["username"]))
 
             else:
-                flash( f'Password incorrect. Please try again.', 'danger')
+                flash('Password incorrect. Please try again.', 'danger')
                 return redirect(url_for('login'))
 
         else:
-            flash(f'Username not found. Please try again.', 'danger')
+            flash('Username not found. Please try again.', 'danger')
         return redirect(url_for('login'))
 
-
-    return render_template('login.html',form=login_form)
+    return render_template('login.html', form=login_form)
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    """
+    function for register extent jinja base.html.
+    Users have to register before login.
+    """
     register_form = RegisterForm()
 
     if register_form.validate_on_submit():
-        found_username = mongo.db.users.find_one({'username': request.form['username']})
+        found_username = mongo.db.users.find_one({'username':
+                                                  request.form['username']})
 
         if not found_username:
             hashed_pw = generate_password_hash(request.form.get("password"))
             mongo.db.users.insert_one({'username': register_form.username.data,
-                              'password': hashed_pw})
+                                       'password': hashed_pw})
             session["username"] = request.form.get("username")
-            return render_template('profile.html',username=session["username"])
-    
+            return render_template('profile.html',
+                                   username=session["username"])
         else:
-            flash(f'Duplicate username detected. Please try again', 'danger')
+            flash('Duplicate username detected. Please try again', 'danger')
             return redirect(url_for('register'))
 
-    return render_template('register.html',form=register_form)
+    return render_template('register.html', form=register_form)
 
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
+    """
+    function for profile extent jinja base.html.
+    Users have to register. Then can access their profile.
+    """
     username = mongo.db.users.find_one(
         {"username": session["username"]})["username"]
     recipes = mongo.db.recipes.find()
@@ -104,6 +130,10 @@ def profile(username):
 
 @app.route("/logout")
 def logout():
+    """
+    function for logout extent jinja base.html.
+    Erase session from website.
+    """
     flash("You have been logged out")
     session.clear()
     return redirect(url_for("home"))
@@ -111,6 +141,10 @@ def logout():
 
 @app.route('/addrecipe', methods=['GET', 'POST'])
 def addrecipe():
+    """
+    function for addrecipe extent jinja base.html.
+    Allow users to add recipes. Use wtfroms.
+    """
     recipe_form = RecipeForm()
 
     if request.method == 'POST':
@@ -123,57 +157,95 @@ def addrecipe():
             'calories': recipe_form.calories.data,
             'recipe_image': recipe_form.recipe_image.data,
         })
-        flash(f'Recipe added.', 'primary')
+        flash('Recipe added.', 'primary')
         return render_template("profile.html")
-    return render_template('newtask.html',form=recipe_form)
+    return render_template('newtask.html', form=recipe_form)
 
 
 @app.route('/recipe_view/<recipe_id>')
 def recipe_view(recipe_id):
+    """
+    function for recipe_view extent jinja base.html.
+    User must be login.
+    """
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template('recipe_view.html', recipe=recipe)
 
 
 @app.route('/edit_recipe/<recipe_id>', methods=['GET', 'POST'])
 def edit_recipe(recipe_id):
+    """
+    function for edit_recipe extent jinja base.html.
+    User must be loged and has to be the creator of recipe.
+    Furthermore, the recipe must exist.
+    """
     recipe_edit = EditRecipeForm()
-
+    recipe = mongo.db.recipes.find_one_or_404({'_id': ObjectId(recipe_id)})
     if request.method == 'POST':
         mongo.db.recipes.update_one({'_id': ObjectId(recipe_id)},
-                                {'$set':
+                                    {'$set':
                                     {
-            'recipe_name': recipe_edit.recipe_name.data,
-            'description': recipe_edit.description.data,
-            'ingredients': recipe_edit.ingredients.data,
-            'cooking_time': recipe_edit.cooking_time.data,
-            'calories': recipe_edit.calories.data,
-            'recipe_image': recipe_edit.recipe_image.data,
-        }})
-        
-        flash(f'Recipe updated.', 'primary')
+                                     'recipe_name': recipe_edit.
+                                     recipe_name.data,
+                                     'description': recipe_edit.
+                                     description.data,
+                                     'ingredients': recipe_edit.
+                                     ingredients.data,
+                                     'cooking_time': recipe_edit.
+                                     cooking_time.data,
+                                     'calories': recipe_edit.
+                                     calories.data,
+                                     'recipe_image': recipe_edit.
+                                     recipe_image.data, }})
+        flash('Recipe updated.', 'primary')
         return render_template("profile.html")
 
-    return render_template('edit_recipe.html', form=recipe_edit)
+    elif request.method == 'GET':
+        recipe_edit.recipe_name.data = recipe['recipe_name']
+        recipe_edit.description.data = recipe['description']
+        recipe_edit.ingredients.data = recipe['ingredients']
+        recipe_edit.cooking_time.data = recipe['cooking_time']
+        recipe_edit.calories.data = recipe['calories']
+        recipe_edit.recipe_image.data = recipe['recipe_image']
+
+    else:
+        flash('Something went wrong...Recipe not updated.', 'primary')
+        return redirect(url_for('profile'))
+
+    return render_template('edit_recipe.html', form=recipe_edit, recipe=recipe)
 
 
 @app.route('/delete_recipe/<recipe_id>', methods=['GET', 'POST'])
 def delete_recipe(recipe_id):
+    """
+    function for delete_recipe extent jinja base.html.
+    User must be loged and has to be the creator of recipe.
+    Furthermore, the recipe must exist.
+    """
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     recipe_delete = DeleteForm()
     if request.method == 'POST':
         if session['username'] == request.form.get('username'):
             mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
-            flash(f'Recipe removed.', 'danger')
+            flash('Recipe removed.', 'danger')
             return render_template("profile.html")
         else:
-            flash(f'Error ocurred.', 'danger')
+            flash('Error ocurred.', 'danger')
             return render_template("profile.html")
 
-    return render_template('delete_recipe.html', form=recipe_delete, recipe = recipe)
+    return render_template('delete_recipe.html', form=recipe_delete,
+                           recipe=recipe)
+
+
+@app.errorhandler(404)
+def response_404(exception):
+    """
+    function for handle exceptions.
+    """
+    return render_template('404.html', exception=exception)
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-
